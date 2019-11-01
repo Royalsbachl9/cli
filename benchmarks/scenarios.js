a383c584a03862db1fbb416fecf986a490dc0e09
Michael Perrotte <mike@npmjs.com>
2019-11-01 14:40:57 -0400

    feat: added benchmarking scenarios [WIP]
    - TODO: still need to add the rest of the scenarios

--- benchmarks/scenarios.js
+++ benchmarks/scenarios.js
@@ -2,9 +2,28 @@
 
 const child = require('child_process')
 const rimraf = require('rimraf')
+const log = require('npmlog')
 const { join } = require('path')
 
-const CACHE_NAME = 'cache'
+const {
+  CACHE_NAME,
+  FIXTURES_DIR
+} = require('./constants')
+
+// Helper Functions
+function createEnv (overrides = {}) {
+  console.group('createEnv') // TESTING
+  const env = Object.keys(process.env).reduce((acc, key) => {
+    return (key.match(/^npm_/))
+      // Don't include `npm_` key/values
+      ? acc
+      // Add key/value to env object
+      : Object.assign({}, acc, { [key]: process.env[key] })
+  }, {})
+  // log.silly('env:', env) // TESTING
+  console.groupEnd('createEnv end') // TESTING
+  return Object.assign({}, env, overrides)
+}
 
 async function removePath (path) {
   return new Promise((resolve, reject) => {
@@ -17,44 +36,52 @@ async function removePath (path) {
   })
 }
 
-async function removeCache (cwd) {
-  const cacheDir = join(cwd, CACHE_NAME)
-  console.group('removeCache')
-  console.log('cacheDir:', cacheDir)
-  console.groupEnd()
-  return Promise.resolve()
-  // return removePath(cacheDir)
+async function removeCache (ctx, fixture) {
+  const cacheDir = join(FIXTURES_DIR, CACHE_NAME)
+  console.group('removeCache') // TESTING
+  log.silly('cacheDir:', cacheDir) // TESTING
+  console.groupEnd('removeCache end') // TESTING
+  // return Promise.resolve()
+  return removePath(cacheDir)
 }
 
-async function removeNodeModules (cwd) {
+async function removeNodeModules (ctx, fixture) {
+  const cwd = join(FIXTURES_DIR, fixture)
   const nodeModulesDir = join(cwd, 'node_modules')
-  console.group('removeNodeModules')
-  console.log('nodeModulesDir:', nodeModulesDir)
-  console.groupEnd()
-  return Promise.resolve()
-  // return removePath(nodeModulesDir)
+  console.group('removeNodeModules') // TESTING
+  log.silly('nodeModulesDir:', nodeModulesDir) // TESTING
+  console.groupEnd('removeNodeModules end') // TESTING
+  // return Promise.resolve()
+  return removePath(nodeModulesDir)
 }
 
-async function removeLockfile (cwd) {
+async function removeLockfile (ctx, fixture) {
   // TODO: change this to use `scenario.lockfile`
+  const cwd = join(FIXTURES_DIR, fixture)
   const lockfilePath = join(cwd, 'package-lock.json')
-  console.group('removeLockfile')
-  console.log('lockfilePath:', lockfilePath)
-  console.groupEnd()
-  return Promise.resolve()
-  // return removePath(lockfilePath)
+  console.group('removeLockfile') // TESTING
+  log.silly('lockfilePath:', lockfilePath) // TESTING
+  console.groupEnd('removeLockfile end') // TESTING
+  // return Promise.resolve()
+  return removePath(lockfilePath)
 }
 
 async function measureAction ({ cmd, args, env, cwd }) {
+  log.silly('executing...') // TESTING
+  log.silly('cmd:', cmd) // TESTING
+  log.silly('args:', args) // TESTING
+  log.silly('env:', env) // TESTING
+  log.silly('cwd:', cwd) // TESTING
   const startTime = Date.now()
-  const delay = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms))
-  await delay(1000)
-  // const result = child.spawnSync(cmd, args, { env, cwd, stdio: 'inherit' })
-  // if (result.status !== 0) {
-  //   console.error(result.error)
-  //   throw new Error(`${cmd} failed with status code ${result.status}`)
-  // }
+  // const delay = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms))
+  // await delay(1000)
+  const result = child.spawnSync(cmd, args, { env, cwd, stdio: 'inherit' })
+  if (result.status !== 0) {
+    console.error(result.error)
+    throw new Error(`${cmd} failed with status code ${result.status}`)
+  }
   const endTime = Date.now()
+  console.groupEnd('Instance Action End')
   return endTime - startTime
 }
 
@@ -69,7 +96,7 @@ module.exports = [
     name: 'fresh install',
     details: {
       cache: false,
-      nodeModules: false,
+      node_modules: false,
       lockfile: false
     },
     cmd: 'npm',
@@ -85,11 +112,12 @@ module.exports = [
       removeCache,
       removeNodeModules,
       removeLockfile,
-      async (cwd, ctx) => {
+      async (ctx, fixture) => {
         const { cmd, args } = ctx
-        const env = {}
+        const env = createEnv()
 
-        console.log('Instance Action') // TESTING
+        const cwd = join(FIXTURES_DIR, fixture)
+        console.group('Instance Action') // TESTING
         return measureAction({ cmd, args, env, cwd })
       }
     ]
@@ -98,7 +126,7 @@ module.exports = [
     name: 'repeat install',
     details: {
       cache: true,
-      nodeModules: true,
+      node_modules: true,
       lockfile: true
     },
     cmd: 'npm',
@@ -111,10 +139,11 @@ module.exports = [
       'https://registry.npmjs.org/'
     ],
     actions: [
-      (cwd, ctx) => {
+      (ctx, fixture) => {
         const { cmd, args } = ctx
-        const env = {}
+        const env = createEnv()
 
+        const cwd = join(FIXTURES_DIR, fixture)
         return measureAction({ cmd, args, env, cwd })
       }
     ]
