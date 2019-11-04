f78cb3272e48b502817cdac506f43a1a38abb183
Michael Perrotte <mike@npmjs.com>
2019-11-04 10:40:32 -0500

    feat: updated constants file with data needed for results

--- benchmarks/constants.js
+++ benchmarks/constants.js
@@ -2,9 +2,13 @@
 
 const { join } = require('path')
 
+const pkg = require('../package.json')
+
 exports.BASE_DIR = join(__dirname, '..')
 exports.BENCHMARK_DIR = join(this.BASE_DIR, 'benchmarks')
 exports.FIXTURES_DIR = join(this.BENCHMARK_DIR, 'fixtures')
 exports.TMP_DIR = join(this.BASE_DIR, 'tmp')
+exports.RESULTS_DIR = join(this.BENCHMARK_DIR, 'results')
 
 exports.CACHE_NAME = 'cache'
+exports.VERSION = pkg.version
